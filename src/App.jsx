4a5babdb3845eb93038f7df25864dd4a05bb1912
TilokDutta <tilokdutta1234@gmail.com>
2026-04-02 18:29:18 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import {Routes,Route,Navigate} from "react-router-dom"
+import LandingPage from './pages/LandingPage'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage/>} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+    return <div>
+        <nav className="bg-blue-100">
+            navbar
+        </nav>
+    </div>
+}
